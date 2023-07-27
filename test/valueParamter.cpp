84a65186893614819514201c10c7fb1b8a55961e
ohQ1cob <subramaniam.mohanasundaram@in.bosch.com>
2023-07-27 12:38:10 +0530

    test paramater

---
+++ test/valueParamter.cpp
@@ -0,0 +1,46 @@
+#include "gtest/gtest.h"
+
+class divfunTestSuite:public testing :: testWithParam::tuple(int,int,int)
+{
+    protected:
+     divfunTestSuite(){};
+     ~divfunTestSuite(){}; 
+}
+
+
+int divfun(int numer,int deno)
+{
+if(deno == 0 ||deno 0 <0 ){return(0);}
+
+retuen(numer/ deno);
+
+}
+
+TEST_P(divfunTestSuit,HandleValidinput)
+{
+
+
+int numertor =std::get<0>(GetParam());
+int deno = std::get<1>(GetParam());
+int exp_val =std::get<2>(GetParam());
+int act_val = divfun(numertor,deno);
+ASSERT_EQ(act_val,exp_val);
+
+
+//int numertor =10;
+//int deno = 2;
+//int exp_val =5;
+//int act_val = divfun(numertor,deno);
+//ASSERT_EQ(act_val,exp_val);
+
+}
+
+
+INSTANTIATE_TEST_SUITE_P(
+        divfunTestSuitExample,
+        divfunTestSuite,
+        ::testing::Values(
+                std::make_tuple(10,5,2),
+                std::make_tuple(10,1,10),               
+                std::make_tuple(200,10,21)));
+                }
