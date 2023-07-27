8a76ddc2adf1e63503c96e5b49b993bb19eb929d
ohQ1cob <subramaniam.mohanasundaram@in.bosch.com>
2023-07-27 16:40:43 +0530

    test typeparameter

--- test/TypeParamvaleExample.cpp
+++ test/TypeParamvaleExample.cpp
@@ -1,70 +1,4 @@
-#if 0
 
-#include "gtest/gtest.h"
-using testing::Types;
-
-class ITempsnr{
-public:
-   virtual int getOutsideTemp() = 0;
-
-};
-class ModelATempsensor:public ITempsnr{
-public:
-int getOutsideTemp() { return 23;}
-
-
-};
-class ModelBTempsensor:public ITempsnr {
-public:
-int getOutsideTemp() { return 23;}
-};
-
-class AutoTepmRe{
-ITempsnr* tempsensorptr;
-public:
- AutoTepmRe(ITempsnr * addr):tempsensorptr{addr}{}
-
- void regulaterTemp()
- {
-this->tempsensorptr->getOutsideTemp();
-
- }
-
-};
-
-
-/*fct func*/
-
-template <typename T>
-ITempsnr* createObject();
-template <>
-ITempsnr* createObject<ModelATempsensor>() { return new ModelATempsensor(); }
-template <>
-ITempsnr* createObject<ModelBTempsensor>() { return new ModelBTempsensor(); }
-template <typename T>
-class TempSensorFixture:public testing::Test{
-protected:
-   TempSensorFixture():objuntertest{createObject<T>()}{}
- ITempsnr * objuntertest;
-};
-
-typedef Types<ModelATempsensor, ModelBTempsensor> Implementations;
-
-
-TYPED_TEST_SUITE(TempSensorFixture, Implementations);
-
-
-
-
-TYPED_TEST(TempSensorFixture, GetTempTest)
-
-{
-
-    ASSERT_EQ(this->objuntertest->getOutsideTemp(), 23);
-
-}
-
-#endif
 #include "gtest/gtest.h"
 
 using testing::Types;
@@ -228,5 +162,22 @@ TYPED_TEST(TempSensorFixture, GetTempTest)
 {
 
     ASSERT_EQ(this->objUnderTest->getOutsideTemp(), 23);
+
+}
+
+class FakeTempSensor:public ITempSensor{
+public:
+ int getOutsideTemp(){
+    return(0);
+ }
+
+};
+
+TEST(AutoTempRegulatorTestSuit,regulateTemp)
+
+{
+    FakeTempSensor stup;
+    AutoTempRegulatorTestSuit codeunderTest(&stup);
+codeunderTest.regulateTemp();
 
 }
