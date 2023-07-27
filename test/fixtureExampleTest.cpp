23ca105916a66ba6eef8cd4574249e693c3f8803
ohQ1cob <subramaniam.mohanasundaram@in.bosch.com>
2023-07-27 11:57:58 +0530

    test fisture

--- test/fixtureExampleTest.cpp
+++ test/fixtureExampleTest.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+
+#if 0
 class C{};
 class B{
  C* ptr;
@@ -29,6 +31,105 @@ TEST(TestfixtureTestsuit,operationTest){
     B bobj(&cobj);
     A obj(&bobj);
     ASSERT_TRUE(obj.operation);    
+
+}
+TEST(TestfixtureTestsuit,operationTest){
+
+   C cobj;
+    B bobj(&cobj);
+    A obj(&bobj);
+    ASSERT_FALSE(obj.operation);    
+
+}
+#endif
+
+class C{};
+class B{
+ C* ptr;
+    public:
+B(C* addressofobject):ptr{addressofobject}
+    {
+        
+    }
+    bool operation()
+    {
+        return(true);
+    }
+
+};
+class A{
+
+    B* ptr;
+    public:
+    A(B* addressofobject):ptr{addressofobject}
+    {
+
+    }     
+};
+
+TEST(TestfixtureTestsuit:public testing::Test){
+
+    protected:
+     class C{};
+class B{
+ C* ptr;
+    public:
+B(C* addressofobject):ptr{addressofobject}
+    {
+        
+    }
+    bool operation()
+    {
+        return(true);
+    }
+
+};
+class A{
+
+    B* ptr;
+    public:
+    A(B* addressofobject):ptr{addressofobject}
+    {
+
+    }     
+};
+
+class TestfixtureTestsuit:public testing::test{
+
+    protected:
+    A* aptr;
+    B* bptr;
+    C* cptr;
+
+void SetUp()
+{
+ this->cptr = new C();
+  this->bptr = new B(cptr);
+   this->aptr = new A(bptr);
+}
+
+void  TearDown()
+{
+delete cptr;
+delete bptr;
+delete aptr;
+
+}
+};
+//Test fixture 
+
+TEST_F(TestfixtureTestsuit,operationTrueTest){
+
+ 
+    ASSERT_TRUE(aptr->operation());    
+
+}
+TEST_F(TestfixtureTestsuit,operationFalseTest){
+
+
+    ASSERT_FALSE(aptr->operation());       
+
+}
 
 }
 TEST(TestfixtureTestsuit,operationTest){
